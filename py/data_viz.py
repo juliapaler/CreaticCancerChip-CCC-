ac892535ab4efbc81294d1b96dcd61a560678dee
gitUmaru <umaru.alia@gmail.com>
2021-05-08 20:01:21 -0700

    data visualization for the data + data

---
+++ py/data_viz.py
@@ -0,0 +1,76 @@
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+for dirname, _, filenames in os.walk('./py/data'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
+    nunique = df.nunique()
+    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]]
+    nRow, nCol = df.shape
+    columnNames = list(df)
+    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
+    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    for i in range(min(nCol, nGraphShown)):
+        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
+        columnDf = df.iloc[:, i]
+        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
+            valueCounts = columnDf.value_counts()
+            valueCounts.plot.bar()
+        else:
+            columnDf.hist()
+        plt.ylabel('counts')
+        plt.xticks(rotation = 90)
+        plt.title(f'{columnNames[i]} (column {i})')
+    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+    plt.show()
+
+
+def plotCorrelationMatrix(df, graphWidth):
+    filename = df.dataframeName
+    df = df.dropna('columns') # drop columns with NaN
+    if df.shape[1] < 2:
+        print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
+        return
+    corr = df.corr()
+    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
+    corrMat = plt.matshow(corr, fignum = 1)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.gca().xaxis.tick_bottom()
+    plt.colorbar(corrMat)
+    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
+    plt.show()
+
+def plotScatterMatrix(df, plotSize, textSize):
+    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
+    df = df.dropna('columns')
+    columnNames = list(df)
+        columnNames = columnNames[:10]
+    df = df[columnNames]
+    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
+    corrs = df.corr().values
+    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
+        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
+    plt.suptitle('Scatter and Density Plot')
+    plt.show()
+
+nRowsRead = 1000
+
+df1 = pd.read_csv('./py/data/Debernardi et al 2020 data.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'Debernardi et al 2020 data.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
+
+print(df1)
+
+def main():
+    plotPerColumnDistribution(df1, 10, 5)
+
+if __name__ == '__main__':
+    main()
